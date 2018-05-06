b4f7692bc42ca1b84d54c67c7e8410672d5e147e
Santiago Leiva <santiagoeleiva@gmail.com>
2018-05-06 19:06:20 -0300

    Pantala de boards

--- src/app/boards/boards.component.ts
+++ src/app/boards/boards.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Board } from '../model/board.model';
+import { BoardService } from '../service/board.service';
+
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html',
-  styleUrls: ['./boards.component.css']
+  styleUrls: ['./boards.component.css'],
+  providers: [BoardService]
 })
 export class BoardsComponent implements OnInit {
 
-  constructor() { }
+  private boards: Array<Board>;
+
+  constructor(private boardService: BoardService) { }
 
   ngOnInit() {
+    this.loadBoards();
   }
 
+  private loadBoards(): void {
+    this.boardService.loadBoards().subscribe(boardsResponse => {
+      this.boards = boardsResponse;
+    });
+  }
 }
--- src/app/service/board.service.ts
+++ src/app/service/board.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { Board } from '../model/board.model';
+
+const url = 'http://localhost:8080/boards';
 
 @Injectable()
 export class BoardService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Obtenemos los boards
+   */
+  public loadBoards(): Observable<Board[]> {
+    return this.http.get<Board[]>(url);
+  }
+
+  /**
+   * Obtenemos los boards de un usuario en base a su username
+   * @param username
+   */
+  public loadUserBoards(username: string): Observable<Board[]> {
+    return this.http.get<Board[]>(url + '/' + username);
+  }
 
 }
